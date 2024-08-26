6fbb9eae4aaf23a0f82240453a258e5c88e8211d
Maximilian Weichart <maximilian.weichart@icloud.com>
2024-08-27 00:26:43 +0200

    Docker script

--- docker-script.sh
+++ docker-script.sh
@@ -1,24 +1,3 @@
-# Set environment variables
-export DEBIAN_FRONTEND=noninteractive
-export TZ=America/San_Francisco
-export PYTHONUNBUFFERED=1
-export PIP_NO_CACHE_DIR=1
-export PIP_ROOT_USER_ACTION=ignore
-export XLA_PYTHON_CLIENT_MEM_FRACTION=0.8
-export GCS_RESOLVE_REFRESH_SECS=60
-export GCS_REQUEST_CONNECTION_TIMEOUT_SECS=300
-export GCS_METADATA_REQUEST_TIMEOUT_SECS=300
-export GCS_READ_REQUEST_TIMEOUT_SECS=300
-export GCS_WRITE_REQUEST_TIMEOUT_SECS=600
-export MUJOCO_GL=egl
-export NUMBA_CACHE_DIR=/tmp
-
-# Update and install dependencies
-apt-get update && apt-get install -y \
-  ffmpeg git vim curl software-properties-common \
-  libglew-dev x11-xserver-utils xvfb \
-  && apt-get clean
-
 # Set up Python 3.11
 add-apt-repository ppa:deadsnakes/ppa
 apt-get update && apt-get install -y python3.11-dev python3.11-venv && apt-get clean
@@ -27,14 +6,12 @@ source ./venv/bin/activate
 pip install --upgrade pip setuptools
 
 # Install agent requirements
+pip install jax[cuda]
 pip install -r dreamerv3/requirements.txt \
   -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 # Install embodied requirements
 pip install -r embodied/requirements.txt
-
-# Set permissions
-chown 1000:root . && chmod 775 .
 
 # Install Tetris-Gymnasium
 pip install opencv-python-headless
@@ -49,4 +26,4 @@ pip install wandb moviepy imageio
 wandb login
 
 # Test JAX
-python3 -c "import jax; print(jax.devices())"
+python3.11 -c "import jax; print(jax.devices())"
